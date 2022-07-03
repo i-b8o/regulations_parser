adb30e175f719fceaa3af6c55ffe42882e6bb4b6
i-b8o <105774619+i-b8o@users.noreply.github.com>
2022-07-03 19:56:45 +0300

    Update scripts.go

--- scripts.go
+++ scripts.go
@@ -1,33 +1,51 @@
 js = `
 // Chapter name
-let chapterName = document.getElementsByTagName("h1")[0].innerText;
+// let chapterName = document.getElementsByTagName("h1")[0].innerText;
 // Chapter num
-let chapterNum = chapterName.split(". ")[0];
+// let chapterNum = chapterName.split(". ")[0];
 // Delete header, all indents  and info links
-document.getElementsByTagName("h1")[0].remove();
-
-Array.prototype.slice.apply( document.getElementsByClassName("info-link") ).map((el) => el.remove());
-Array.prototype.slice.apply( document.getElementsByClassName("no-indent") ).map((el) => el.remove());
+// document.getElementsByTagName("h1")[0].remove();
+// Array.prototype.slice.apply( document.getElementsByClassName("info-link") ).map((el) => el.remove());
+// Array.prototype.slice.apply( document.getElementsByClassName("no-indent") ).map((el) => el.remove());
 let paragraphs = [];
-
-let pArray = Array.prototype.slice.apply( document.getElementsByTagName("p") );
-pArray.map(function(p){
+let content = document.getElementsByClassName("document-page__content")[0];
+content.childNodes.forEach(function(el){
+    console.log(el);
+    // If table
+    if (el.classList && el.classList.contains("doc-table")){
+        let paragraph = {};
+        el.getElementsByTagName("table")[0].setAttribute('style', '');
+        paragraph.text = el.innerHTML;
+        paragraphs.push(paragraph);
+        return;
+    };
+    
+    // Drop useless elements
+    if (el.tagName != 'DIV' && el.tagName != 'P'){
+            return;
+    }
+    if(el.classList && el.classList.contains("document__style")){return};
+    if(el.classList && el.classList.contains("no-indent")){return};
+    
     let paragraph = {};
+  
     // Paragraph Id
-    let pIdTag = p.getElementsByTagName("a")[0];
+    let pIdTag = el.getElementsByTagName("a")[0];
+    
     let pId = pIdTag.id.split("dst")[1];
     paragraph.id = parseInt(pId);
     pIdTag.remove();
+    let divTag = el.getElementsByClassName("info-link")[0];
+    if (divTag){divTag.remove()}
     // Paragraph text
     // if inner contains links prepair them
-    let links = Array.prototype.slice.apply( p.getElementsByTagName("a") );
+    let links = Array.prototype.slice.apply( el.getElementsByTagName("a") );
     if (links.length > 0){
         links.forEach((el) => el.href = el.href.split("#dst")[1]);
     }
-
-    paragraph.text = p.innerHTML;
+    paragraph.text = el.innerHTML;
     
-    console.log(paragraph);
+    paragraphs.push(paragraph);
 });
 
 `
